de4ce3bf9cf383914885b4d6b3a1dbc14d222dd6
p_tsanev <plamen_canev@abv.bg>
2022-09-24 20:59:08 +0300

    working MySQL database

---
+++ server/Database/users.js
@@ -0,0 +1,8 @@
+const db = require('mysql2');
+
+module.exports = db.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Lolzed1234',
+    database: 'Geekscord'
+});
--- server/server.js
+++ server/server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express();
 
+const db = require('./Database/users.js');
+
 app.use(express.json());
 
 app.post('/register', (req, res) => {
     if(Object.keys(req.body).length !== 0){
+        const {username, password, email} = req.body;
         console.log('Registered: ', req.body);
         res.json(req.body);
+        db.promise().query(`INSERT INTO users (Username, Password, Email) VALUES ("${username}", "${password}", "${email}")`);
+        db.promise().query('SELECT * FROM users')
+        .then((results, fields) => console.log('Database: ', results[0]));
     }
 });
 
@@ -14,6 +20,8 @@ app.post('/login', (req, res) => {
     if(Object.keys(req.body).length !== 0){
         console.log('Logged in: ', req.body);
         res.json(req.body);
+        db.promise().query('SELECT * FROM users')
+        .then((results, fields) => console.log('Database: ', results[0]));
     }
 });
 
